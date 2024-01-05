bad9dce0ca0c05297b96f5575fc4efb8780782c4
wbdvlpmnt <jai.gokhale5219@gmail.com>
2024-01-05 16:40:10 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace variableAndConstants;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        byte number = 2;
+        Console.WriteLine(number);
+    }
+}
