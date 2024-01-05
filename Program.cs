1d3c4c22e12851bd81a00d388eddcecb3dc722f6
wbdvlpmnt <jai.gokhale5219@gmail.com>
2024-01-05 16:52:59 -0600

    this is a format string, how much can we store

--- Program.cs
+++ Program.cs
@@ -5,19 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        byte number = 2;
-        int count = 10;
-        float totalPrice = 20.95f; // treat as float without we recieve error
-        char charecter = 'A'; // single quote for charecter
-        string firstName = "Jai"; // double quote for string
-        // bool isWorking = false;
-        var figureOutTheType = 2;
-
-        Console.WriteLine(number);
-        Console.WriteLine(count);
-        Console.WriteLine(totalPrice);
-        Console.WriteLine(charecter);
-        Console.WriteLine(firstName);
+        System.Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
 
     }
 }
